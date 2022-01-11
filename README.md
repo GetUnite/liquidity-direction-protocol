0f49a94070e0f0fd49359c627539f53d71c92ed3
Alexander Malyshev <alexanderem49@gmail.com>
2022-01-11 16:37:20 +0700

    Project initialized

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alluo Protocol Project
+
+This repository contains smart contracts of Alluo Protocol
---
+++ hardhat.config.ts
@@ -0,0 +1,65 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import 'hardhat-contract-sizer'
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 999999999,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      forking: {
+        url: process.env.POLYGON_FORKING_URL as string,
+      },
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      gasPrice: "auto",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+    maticmainnet: {
+      url: process.env.POLYGON_URL,
+      gasPrice: "auto",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 3600000,
+  },
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: false,
+  }
+};
+
+export default config;
