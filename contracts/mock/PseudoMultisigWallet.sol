fc6c16c7cb619fd757afe8f4b2aff125bc8fceeb
Alexander Malyshev <alexanderem49@gmail.com>
2022-01-13 15:02:07 +0700

    Feature: pseudo multisig wallet for tests

---
+++ contracts/mock/PseudoMultisigWallet.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract PseudoMultisigWallet {
+    using Address for address;
+
+    constructor() {
+        uint256 id;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            id := chainid()
+        }
+
+        // solhint-disable-next-line reason-string
+        require(
+            id == 1337 || id == 31337,
+            "Do not deploy this contract on public networks!"
+        );
+    }
+
+    function executeCall(address destination, bytes calldata _calldata)
+        external
+        returns (bytes memory)
+    {
+        return destination.functionCall(_calldata);
+    }
+}
