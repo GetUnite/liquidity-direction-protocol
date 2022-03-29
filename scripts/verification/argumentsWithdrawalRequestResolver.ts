3a720b6e5e136f8cccbe2487e21ba1bbdee56c8f
zayk00v <zayk00vart@gmail.com>
2022-03-29 13:21:49 +0500

    Fix: terminal commands for new structure

--- scripts/verification/argumentsWithdrawalRequestResolver.ts
+++ scripts/verification/argumentsWithdrawalRequestResolver.ts
@@ -3,5 +3,5 @@ module.exports = [
     "0xa248Ba96d72005114e6C941f299D315757877c0e", //Liquidity buffer on mainnet matic
     "0x0B74fFf7D99E524319D59f4dC399413c1E4E1A93" //Gnosis address, also admin
   ];
-//| => npx hardhat verify --constructor-args scripts/argumentsWithdrawalRequestResolver.ts 0x6267ead8D01A30C56943789eB324EfDE36c51Df0 --network mumbai
-//| => npx hardhat verify --constructor-args scripts/argumentsWithdrawalRequestResolver.ts 0x6267ead8D01A30C56943789eB324EfDE36c51Df0 --network polygon
+//| => npx hardhat verify --constructor-args scripts/verification/argumentsWithdrawalRequestResolver.ts 0x6267ead8D01A30C56943789eB324EfDE36c51Df0 --network mumbai
+//| => npx hardhat verify --constructor-args scripts/verification/argumentsWithdrawalRequestResolver.ts 0x6267ead8D01A30C56943789eB324EfDE36c51Df0 --network polygon
