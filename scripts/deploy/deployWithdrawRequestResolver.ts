f258b5a086d542880320b0d05a6ed140bb602b1a
0xtuytuy <6547517+0xtuytuy@users.noreply.github.com>
2022-03-29 15:01:47 +0200

    Typo in file name

--- scripts/deploy/deployWithdrawRequestResolver.ts
+++ scripts/deploy/deployWithdrawRequestResolver.ts
@@ -1,9 +1,9 @@
 import { ethers} from "hardhat"
 
-const deploymentArguments = require('../verification/argumentsWithdrawalRequestResolver');
+const deploymentArguments = require('../verification/argumentsWithdrawalRequestResolver.ts');
 
 async function main() {
-    const WithdrawawlRequestResolver = await ethers.getContractFactory("WithdrawawlRequestResolver");
+    const WithdrawawlRequestResolver = await ethers.getContractFactory("WithdrawalRequestResolver");
 
     let withdrawawlRequestResolver = await WithdrawawlRequestResolver.deploy(deploymentArguments[0],deploymentArguments[1],deploymentArguments[2])
 
