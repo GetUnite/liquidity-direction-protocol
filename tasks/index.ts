1768612508d549654453c0cc9b9d69d81a26af43
zayk00v <zayk00vart@gmail.com>
2022-04-16 13:58:20 +0500

    Feature: task for token holders list

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,6 +1,7 @@
 import './dev/addDepositToken'
 import "./dev/grantRole"
 import './dev/migrate'
+import './dev/holders'
 import "./voting/createEntry"
 import "./voting/getPoolId"
 import "./migration/claimAll"
