ab21a637f2f2a97674a7327a8a1a6d92b817b875
zayk00v <zayk00vart@gmail.com>
2022-04-18 18:29:24 +0500

    Feature: all scripts and apy change for deploy

--- tasks/migration/claimAll.ts
+++ tasks/migration/claimAll.ts
@@ -5,8 +5,9 @@ import { BigNumber } from 'ethers';
 import { config as dotenvConfig } from 'dotenv';
 import { formatBytes32String, parseBytes32String } from "@ethersproject/strings";
 import { Bytes } from "@ethersproject/bytes";
+import { getHolders } from "../../scripts/dev/getHolders";
 
-task("claimAll", "claim all balances from old alluoLp")
+task("claimAll", "claim all balances on old alluoLp")
     .setAction(async function (taskArgs, hre) {
 
         const network = hre.network.name;
@@ -15,19 +16,12 @@ task("claimAll", "claim all balances from old alluoLp")
 
         const alluolp = await hre.ethers.getContractAt("AlluoLpV3", "0x29c66CF57a03d41Cfe6d9ecB6883aa0E2AbA21Ec");
 
-        let list: string[] = [
-            '0x4b948c0354c82f1dc3c510bfa93578540dab917d', 
-            '0xc24444c91a370151bdb06a0c11da8b1006fe1bbe', 
-            '0xfb7a51c6f6a5116ac748c1adf4d4682c3d50889e',
-            '0x9c205edd78bd7ea0a940847f1f98d5822f126e67',
-            '0x76d885d38c219895758eaf45449bb4ed8f664f31'
-        ];
+        let holders: string[] = await getHolders(hre)
         
-        let i =0;
-        while(i < list.length){
-            await alluolp.claim(list[i]);
-            console.log("Claimed for:", list[i]);
-            i++;
+        for(let i = 0; i < holders.length; i++){
+            // console.log(await alluolp.estimateGas.claim(holders[i]));
+            await alluolp.claim(holders[i]);
+            console.log("Claimed for:", holders[i]);
         }
 
         console.log('claimAll task Done!');
--- tasks/migration/lastStep.ts
+++ tasks/migration/lastStep.ts
@@ -7,7 +7,7 @@ import { formatBytes32String, parseBytes32String } from "@ethersproject/strings"
 import { Bytes } from "@ethersproject/bytes";
 import { getHolders } from "../../scripts/dev/getHolders";
 
-task("mintNew", "mint tokens on new version")
+task("last", "mint tokens on new version")
     .setAction(async function (taskArgs, hre) {
 
         const network = hre.network.name;
@@ -15,18 +15,8 @@ task("mintNew", "mint tokens on new version")
         console.log(network);
 
         const ibAlluo = await hre.ethers.getContractAt("IbAlluoUSD", "address");
-        let alluoLp = "0x29c66CF57a03d41Cfe6d9ecB6883aa0E2AbA21Ec"
 
-        let holders: string[] = await getHolders(hre)
+        await ibAlluo.migrateStep2()
 
-        const middleIndex = Math.ceil(holders.length / 2);
-
-        const firstHalf = holders.splice(0, middleIndex);   
-        const secondHalf = holders.splice(-middleIndex);
-
-        await ibAlluo.migrateStep1(alluoLp, firstHalf)
-
-        await ibAlluo.migrateStep1(alluoLp, secondHalf)
-
-        console.log('mintNew task Done!');
+        console.log('last task Done!');
     });
