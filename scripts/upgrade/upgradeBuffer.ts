7d78e4aa793a1a1e2cb4ed89f1ecd672221f4271
Alexander Malyshev <alexanderem49@gmail.com>
2022-05-13 15:17:46 -0300

    Fix: correctly upgraded buffer, new stables deploy

--- scripts/upgrade/upgradeBuffer.ts
+++ scripts/upgrade/upgradeBuffer.ts
@@ -5,7 +5,7 @@ async function main() {
 
   const Buffer = await ethers.getContractFactory("LiquidityBufferVaultForMumbai");
 
-  await upgrades.upgradeProxy('0x20bb70c55387Bc0F9073D27d09Ab0ce9D26c4Eaa', Buffer);
+  await upgrades.upgradeProxy('0xF9F9381Fbc5225180015b1f0eab6c33DbF0b37Ab', Buffer);
   console.log('Buffer upgraded');
 
 }
