a7a87ab6da5e4c2b0f5e25ed07d870d39e7a1ac4
Alexander Malyshev <alexanderem49@gmail.com>
2022-05-13 11:44:26 -0300

    Fix: format and update testnet FakeCurve contract

--- contracts/mock/testnet/FakeCurve.sol
+++ contracts/mock/testnet/FakeCurve.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "../../Farming/AlluoERC20Upgradable.sol";
+import "./../../Farming/AlluoERC20Upgradable.sol";
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
@@ -12,12 +12,12 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
 
-contract FakeCurve is 
-    Initializable, 
-    PausableUpgradeable, 
-    AlluoERC20Upgradable, 
-    AccessControlUpgradeable, 
-    UUPSUpgradeable 
+contract FakeCurve is
+    Initializable,
+    PausableUpgradeable,
+    AlluoERC20Upgradable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
 {
     using AddressUpgradeable for address;
     using SafeERC20Upgradeable for IERC20Upgradeable;
@@ -59,59 +59,54 @@ contract FakeCurve is
         DAI = IERC20Upgradeable(_dai);
         USDC = IERC20Upgradeable(_usdc);
         USDT = IERC20Upgradeable(_usdt);
-
     }
 
-    function add_liquidity(address _token, uint256 _amount) external{
+    function add_liquidity(address _token, uint256 _amount) external {
         if (IERC20Upgradeable(_token) == USDT) {
-            uint256 amountIn18 = _amount * 10 ** 12;
+            uint256 amountIn18 = _amount * 10**12;
             USDT.transferFrom(msg.sender, wallet, _amount);
             _mint(msg.sender, amountIn18);
-
         } else if (IERC20Upgradeable(_token) == DAI) {
             DAI.transferFrom(msg.sender, wallet, _amount);
             _mint(msg.sender, _amount);
         } else if (IERC20Upgradeable(_token) == USDC) {
-           
-            uint256 amountIn18 = _amount * 10 ** 12;
+            uint256 amountIn18 = _amount * 10**12;
             USDC.transferFrom(msg.sender, wallet, _amount);
             _mint(msg.sender, amountIn18);
         }
     }
 
-    function remove_liquidity(address _token, uint256 _amount) external returns(uint256){
+    function remove_liquidity(address _token, uint256 _amount)
+        external
+        returns (uint256)
+    {
         if (IERC20Upgradeable(_token) == USDT) {
             _burn(msg.sender, _amount);
-            uint256 amountIn6 = _amount / 10 ** 12;
-            uint256 amountWithFee = amountIn6 * (10000 - usdtFee) / 10000;
+            uint256 amountIn6 = _amount / 10**12;
+            uint256 amountWithFee = (amountIn6 * (10000 - usdtFee)) / 10000;
             USDT.transferFrom(wallet, msg.sender, amountWithFee);
             return amountWithFee;
-
         } else if (IERC20Upgradeable(_token) == DAI) {
             _burn(msg.sender, _amount);
             DAI.transferFrom(wallet, msg.sender, _amount);
             return _amount;
-
         } else if (IERC20Upgradeable(_token) == USDC) {
             _burn(msg.sender, _amount);
-            uint256 amountIn6 = _amount / 10 ** 12;
-            uint256 amountWithFee = amountIn6 * (10000 - usdcFee) / 10000;
+            uint256 amountIn6 = _amount / 10**12;
+            uint256 amountWithFee = (amountIn6 * (10000 - usdcFee)) / 10000;
             USDC.transferFrom(wallet, msg.sender, amountWithFee);
             return amountWithFee;
         }
     }
 
-
     function setWallet(address newWallet)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-
         wallet = newWallet;
-
     }
 
-    function changeFee(uint usdt, uint usdc)
+    function changeFee(uint256 usdt, uint256 usdc)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
@@ -137,8 +132,7 @@ contract FakeCurve is
 
     function _authorizeUpgrade(address newImplementation)
         internal
-        onlyRole(UPGRADER_ROLE)
         override
-    {
-    }
+        onlyRole(UPGRADER_ROLE)
+    {}
 }
