5fe0adbcbf17d6593b07b6aabdf7ac604c99fef7
RT <rt@alluo.io>
2022-06-02 10:26:34 +0100

    Deployed files to mainnet

--- scripts/upgrade/upgradeHandler.ts
+++ scripts/upgrade/upgradeHandler.ts
@@ -5,7 +5,7 @@ async function main() {
 
   const Handler = await ethers.getContractFactory("LiquidityHandler");
 
-  await upgrades.upgradeProxy('address', Handler);
+  await upgrades.upgradeProxy('0x31a3439ac7e6ea7e0c0e4b846f45700c6354f8c1', Handler);
   console.log('Handler upgraded');
 
 }
