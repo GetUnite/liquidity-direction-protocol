04d734a14fc620b1aece752e4771fc2b10b37bc8
RT <rt@alluo.io>
2022-06-03 10:19:50 +0100

    finished and deployed

--- contracts/Farming/resolvers/IbAlluoPriceResolver.sol
+++ contracts/Farming/resolvers/IbAlluoPriceResolver.sol
@@ -2,35 +2,44 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import "./../../interfaces/IIbAlluo.sol";
+import "./../LiquidityHandler.sol";
 
-contract IbAlluoUSDPriceResolver is AccessControl {
-    address public ibAlluoAddress;
+contract IbAlluoPriceResolver is AccessControl {
+    address[] public ibAlluoAddress;
     address public alluoBank;
-    event IbAlluoUSDValue(
+    address public liquidityHandlerAddress;
+
+    event IbAlluoValue(
         address indexed user,
+        string symbol,
         uint256 usdValue,
         uint256 amountOfIbAlluo,
         uint256 oneIbAlluoValue
     );
 
     constructor(
-        address _ibAlluoAddress,
+        address _liquidityHandlerAddress,
         address _newAdmin,
         address _alluoBank
-    ) public {
+    ) {
         _grantRole(DEFAULT_ADMIN_ROLE, _newAdmin);
-        ibAlluoAddress = _ibAlluoAddress;
+        liquidityHandlerAddress = _liquidityHandlerAddress;
         alluoBank = _alluoBank;
     }
 
     function emitter() external {
-        IIbAlluo(ibAlluoAddress).updateRatio();
-        emit IbAlluoUSDValue(
-            alluoBank,
-            IIbAlluo(ibAlluoAddress).getBalance(alluoBank),
-            IIbAlluo(ibAlluoAddress).balanceOf(alluoBank),
-            IIbAlluo(ibAlluoAddress).growingRatio()
-        );
+        address[] memory iBAlluos  = LiquidityHandler(liquidityHandlerAddress).getListOfIbAlluos();
+        for (uint i=0; i<iBAlluos.length; i++) {
+            IIbAlluo(iBAlluos[i]).updateRatio();
+            emit IbAlluoValue(
+                alluoBank,
+                IERC20Metadata(iBAlluos[i]).symbol(),
+                IIbAlluo(iBAlluos[i]).getBalance(alluoBank),
+                IIbAlluo(iBAlluos[i]).balanceOf(alluoBank),
+                IIbAlluo(iBAlluos[i]).growingRatio()
+            );
+        }
     }
 }
