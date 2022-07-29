6f99482e957f1ee40b3bdf3391c93a93b643c08e
RT <rt@alluo.io>
2022-07-29 12:23:03 +0100

    published version

--- scripts/deploy/deployVoteExecutorResolver.ts
+++ scripts/deploy/deployVoteExecutorResolver.ts
@@ -3,7 +3,7 @@ import { ethers, upgrades } from "hardhat"
 import { VoteExecutorResolver } from "../../typechain";
 
 async function main() {
-    const VoteExecutorMaster = "0x6635Ab05a9D36bb3480C3dbA9b8c506C44CC8880"
+    const VoteExecutorMaster = "0x4Fd58328C2e0dDa1Ea8f4C70321C91B366582eA2"
     const ResolverFactory = await ethers.getContractFactory("VoteExecutorResolver");
     const Resolver = await ResolverFactory.deploy(VoteExecutorMaster);
     console.log("Deployed at:", Resolver.address);
