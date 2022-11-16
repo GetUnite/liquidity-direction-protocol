406afe5b7a97722b3f783de2a7f7c5bb9525714c
zayk00v <zayk00vart@gmail.com>
2022-11-16 19:21:59 +0300

    better script

--- scripts/upgrade/upgradeIbAlluos.ts
+++ scripts/upgrade/upgradeIbAlluos.ts
@@ -2,10 +2,9 @@ import { ethers, upgrades } from "hardhat"
 
 async function main() {
 
-  const IbAlluoOld = await ethers.getContractFactory("IbAlluoOld");
+  // const IbAlluoOld = await ethers.getContractFactory("IbAlluoOld");
 
-  await upgrades.forceImport("0xC2DbaAEA2EfA47EBda3E572aa0e55B742E408BF6", IbAlluoOld);
-
+  // await upgrades.forceImport("0xC2DbaAEA2EfA47EBda3E572aa0e55B742E408BF6", IbAlluoOld);
 
   const IbAlluoNew = await ethers.getContractFactory("IbAlluo");
 
@@ -25,19 +24,8 @@ async function main() {
 
   const StIbAlluoNew = await ethers.getContractFactory("StIbAlluo");
 
-  let StIbAlluoUsd = await ethers.getContractAt("StIbAlluoNew", "0xE9E759B969B991F2bFae84308385405B9Ab01541");
-  let StIbAlluoEur = await ethers.getContractAt("StIbAlluoNew", "0xe199f1B01Dd3e8a1C43B62279FEb20547a2EB3eF");
-  let StIbAlluoEth = await ethers.getContractAt("StIbAlluoNew", "0x2D4Dc956FBd0044a4EBA945e8bbaf98a14025C2d");
-  let StIbAlluoBtc = await ethers.getContractAt("StIbAlluoNew", "0x3E70E15c189e1FFe8FF44d713605528dC1701b63");
+  let stIbAlluoNew = await StIbAlluoNew.deploy()
 
-  // await upgrades.upgradeProxy(StIbAlluoUsd.address, StIbAlluoNew);
-  // console.log('StIbAlluoUsd upgraded');
-  // await upgrades.upgradeProxy(StIbAlluoEur.address, StIbAlluoNew);
-  // console.log('StIbAlluoEur upgraded');
-  // await upgrades.upgradeProxy(StIbAlluoEth.address, StIbAlluoNew);
-  // console.log('StIbAlluoEth upgraded');
-  // await upgrades.upgradeProxy(StIbAlluoBtc.address, StIbAlluoNew);
-  // console.log('StIbAlluoBtc upgraded');
 }
 
 main()
