4f95c07b5d21176c0c14a7a67a9653ebaa3d1295
Alexander Malyshev <alexanderem49@gmail.com>
2022-11-25 16:04:10 +0400

    Fix: remove test, upgrade script

---
+++ scripts/tokenomicsMigrations/upgradeLockers.ts
@@ -0,0 +1,24 @@
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+    const vlAlluoFactory = await ethers.getContractFactory("AlluoLockedV4");
+    const cvxDistributorFactory = await ethers.getContractFactory("CvxDistributorV2");
+
+    const vlAlluoAddress = "0xdEBbFE665359B96523d364A19FceC66B0E43860D";
+    const cvxDistributorAddress = "0xc22DB2874725B84e99EC0a644fdD042EA3F6F899";
+
+    await upgrades.upgradeProxy(
+        vlAlluoAddress,
+        vlAlluoFactory
+    );
+    
+    await upgrades.upgradeProxy(
+        cvxDistributorAddress,
+        cvxDistributorFactory
+    );
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
