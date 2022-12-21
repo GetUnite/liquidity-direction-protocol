505d2607995ddaa3d8c3fe05c94fd1ec8e599865
Alexander Malyshev <alexanderem49@gmail.com>
2022-12-22 03:29:27 +0400

    Feature: ibAlluo setup complete

--- test/farming/ibAlluoWithOracles.ts
+++ test/farming/ibAlluoWithOracles.ts
@@ -41,7 +41,7 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
                     enabled: true,
                     jsonRpcUrl: process.env.POLYGON_FORKING_URL as string,
                     //you can fork from last block by commenting next line
-                    blockNumber: 37109246,
+                    blockNumber: 37116107,
                 },
             },],
         });
@@ -64,6 +64,7 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
         ibAlluoBTC = await ethers.getContractAt("IbAlluo", "0xf272Ff86c86529504f0d074b210e95fc4cFCDce2");
         liquidityHandler = await ethers.getContractAt("LiquidityHandler", "0x31a3439Ac7E6Ea7e0C0E4b846F45700c6354f8c1");
 
+        // Step 1: set up upgrade roles and statuses for all ibAlluos
         await ibAlluoUSD.connect(admin).grantRole(
             await ibAlluoUSD.UPGRADER_ROLE(),
             signers[0].address
@@ -94,6 +95,7 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
         );
         await liquidityHandler.connect(admin).changeUpgradeStatus(true);
 
+        // Step 2: execute upgrades
         await upgrades.forceImport(ibAlluoUSD.address, OldIbAlluoFactory);
         await upgrades.upgradeProxy(ibAlluoUSD.address, IbAlluo);
 
@@ -109,6 +111,7 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
         await upgrades.forceImport(liquidityHandler.address, OldLiquidityHandler);
         await upgrades.upgradeProxy(liquidityHandler.address, LiquidityHandler);
 
+        // Step 3: deploy upgradeable adapters
         btcAdapter = await upgrades.deployProxy(
             BtcNoPoolAdapter,
             [
@@ -147,9 +150,11 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
             { initializer: "initialize", kind: "uups" }
         ) as UsdCurveAdapterUpgradeable;
 
+        // Step 4: call `adapterApproveAll` on USD and EUR adapters
         await usdAdapter.connect(admin).adapterApproveAll();
         await eurAdapter.connect(admin).adapterApproveAll();
 
+        // Step 5: call `setAdapter` on handler with all adapters
         await liquidityHandler.connect(admin).setAdapter(
             1,
             "USD Curve-3pool",
@@ -182,17 +187,10 @@ describe("IbAlluo With Price Oracles (Integration Tests)", async () => {
             true
         );
 
-        let adminRole = ethers.constants.HashZero;
-
-        await liquidityHandler.connect(admin).grantRole(adminRole, ibAlluoUSD.address)
-        await liquidityHandler.connect(admin).grantRole(adminRole, ibAlluoEUR.address)
-        await liquidityHandler.connect(admin).grantRole(adminRole, ibAlluoETH.address)
-        await liquidityHandler.connect(admin).grantRole(adminRole, ibAlluoBTC.address)
-            
-        await liquidityHandler.connect(admin).setIbAlluoToAdapterId(ibAlluoUSD.address, 1)
-        await liquidityHandler.connect(admin).setIbAlluoToAdapterId(ibAlluoEUR.address, 2)
-        await liquidityHandler.connect(admin).setIbAlluoToAdapterId(ibAlluoETH.address, 3)
-        await liquidityHandler.connect(admin).setIbAlluoToAdapterId(ibAlluoBTC.address, 4)      
+        // Step 6: set PriceFeedRouterV2
+        const priceFeedRouterV2 = "0x82220c7Be3a00ba0C6ed38572400A97445bdAEF2"
+        await ibAlluoUSD.connect(admin).setPriceRouterInfo(priceFeedRouterV2, 0);
+        await ibAlluoEUR.connect(admin).setPriceRouterInfo(priceFeedRouterV2, 2);
     });
 
     it("Should allow beforeEach to pass", async () => {
