80f9c92381be276a535446735dd1eb2a990f20f3
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2022-12-22 09:27:31 +0200

    Fix: refill function, tests for BufferManager

--- contracts/interfaces/IBufferManager.sol
+++ contracts/interfaces/IBufferManager.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.11;
 
 interface IBufferManager {
+    function refillBuffer(address _ibAlluo) external returns (bool);
 
-    function refillBuffer(address _ibAlluo) external returns(bool);
+    function isAdapterPendingWithdrawal(
+        address _ibAlluo
+    ) external returns (bool);
 
-    function isAdapterPendingWithdrawal(address _ibAlluo) external returns(bool);
-
-    function adapterRequiredRefill(address _ibAlluo) external returns(uint256);
-
+    function adapterRequiredRefill(address _ibAlluo) external returns (uint256);
 }
